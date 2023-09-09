0e123a3a3f87212c75bb17b2b253bcdcc1d2fd54
Reza01010 <hi95534reza@gmail.com>
2023-09-09 11:17:29 +0430

    .

--- payment/views.py
+++ payment/views.py
@@ -11,51 +11,7 @@ from orders.models import Order
 
 
 
-def payment_callback(request):
-    payment_authority = request.GET.get("Authority")
-    payment_status = request.GET.get("Status")
 
-    order = get_object_or_404(Order, zarinpal_authority=payment_authority)
-
-    toman_total_price = order.get_total_price()
-    rial_total_price = toman_total_price * 10
-
-    if payment_status == 'OK':
-
-        request_header = {
-            "accept": "application/json",
-            "content-type": "application/json"
-        }
-
-        request_data = {
-            'merchant_id': settings.ZARINPAL_MERCHANT_ID,
-            'amount': rial_total_price,
-            'authority':payment_authority,
-
-        }
-        res = requests.post(url='https://api.zarinpal.com/pg/v4/payment/verify.json', data=json.dumps(request_data),
-                            headers=request_header, )
-
-        if 'data' in res.json() and 'errors' not in res.json()['data'] or len(res.json()['data']['errors']) == 0:
-            data = res.json()['data']
-            payment_code = data['code']
-
-            if payment_code == 100:
-                order.is_paid = True
-                order.zarinpal_ref_id = data['ref_id']
-                order.zarinpal_data = data
-                order.save()
-
-                return HttpResponse('پرداخت با موفقیت انجام شد.')
-            elif payment_code ==101:
-                return HttpResponse('پرداخت با موفقیت انجام شد. البته این تراکنش قبلا ثبت شده!')
-
-            else:
-                error_code = res.json()['errors']['code']
-                error_message = res.json()['errors']['message']
-                return HttpResponse(f'تراکنش ناموفق بود.  {error_code}     {error_message}')
-    else:
-        return HttpResponse('تراکنش ناموفق بود.')
 
 
 
